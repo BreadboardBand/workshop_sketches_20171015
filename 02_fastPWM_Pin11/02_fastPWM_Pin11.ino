f3c0798ab802e4502cd6591402a3be118dccf2cf
SAITA Kazuki <saita@kinoshita-lab.org>
2017-10-14 19:19:15 +0900

    add 01_fastPWM_Pin11

---
+++ 02_fastPWM_Pin11/02_fastPWM_Pin11.ino
@@ -0,0 +1,20 @@
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+void setup() {
+ // Timer2 PWM Mode set to fast PWM 
+  cbi (TCCR2A, COM2A0);
+  sbi (TCCR2A, COM2A1);
+  sbi (TCCR2A, WGM20);
+  sbi (TCCR2A, WGM21);
+  cbi (TCCR2B, WGM22);
+  sbi (TCCR2B, CS20);
+  cbi (TCCR2B, CS21);
+  cbi (TCCR2B, CS22);
+  sbi(DDRB,3); 
+  cbi (TIMSK0,TOIE0);    
+}
+
+void loop() {
+ OCR2A = 127 ; // OCR2A という変数に 0 ~ 256までの値を入れると、
+                             // pin11からPWMが出力される
+}
